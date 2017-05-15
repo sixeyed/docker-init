a82d2e129914d1d9c6d29047f93fdabb8fb51f57
Elton Stoneman <elton@sixeyed.com>
2017-05-15 11:39:44 +0100

    Add Ubuntu CE init

---
+++ ubuntu/install-docker-ce.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get -y install \
+  apt-transport-https \
+  ca-certificates \
+  curl
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       edge"
+
+sudo apt-get update
+
+sudo apt-get -y install docker-ce
+
+sudo docker version
