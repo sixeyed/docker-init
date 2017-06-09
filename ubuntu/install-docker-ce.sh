2aff44db371898a021bfbefd4c3685ffbf39c58f
Stefan Scherer <scherer_stefan@icloud.com>
2017-06-09 19:59:18 +0200

    Add CHANNEL variable

--- ubuntu/install-docker-ce.sh
+++ ubuntu/install-docker-ce.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+CHANNEL=${CHANNEL:-edge}
+
 sudo apt-get -y install \
   apt-transport-https \
   ca-certificates \
   curl
+
+sudo apt-get remove -y docker-engine docker.io || true
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
        $(lsb_release -cs) \
-       edge"
+       $CHANNEL"
 
 sudo apt-get update
 
