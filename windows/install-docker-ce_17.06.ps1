fa7a75fc2593f67d42553cfbab126eb1e92940b9
Elton Stoneman <elton@sixeyed.com>
2017-07-13 16:28:21 -0700

    Add 17.06 install for Windows

---
+++ windows/install-docker-ce_17.06.ps1
@@ -0,0 +1,31 @@
+$ProgressPreference = 'SilentlyContinue'
+
+$version = '17.06.0-ce'
+Write-Output "-Installing Docker version: $version"
+
+$service = Get-Service 'docker' -ErrorAction SilentlyContinue
+if ($service) {
+    Write-Output '--Stopping Docker Windows service'
+    Stop-Service docker
+}
+
+$downloadUrl = "https://download.docker.com/win/static/test/x86_64/docker-$version.zip"
+$outFilePath = "$env:TEMP\docker.zip"
+Write-Output "--Downloading: $downloadUrl"
+Invoke-WebRequest -UseBasicParsing -OutFile $outFilePath -Uri $downloadUrl
+Expand-Archive -Path $outFilePath -DestinationPath $env:ProgramFiles -Force
+Remove-Item $outFilePath
+
+if ($service) {
+    Start-Service docker
+    Write-Output '--Starting Docker Windows service'
+}
+else {
+    $env:PATH = $env:ProgramFiles + '\docker;' + $env:PATH
+    [Environment]::SetEnvironmentVariable('PATH', $env:PATH, [EnvironmentVariableTarget]::Machine)
+}
+
+Write-Output '-Done'
+Write-Output '*'
+
+& docker version
---
+++ windows/install-docker-compose_1.14.ps1
@@ -0,0 +1,15 @@
+$ProgressPreference = 'SilentlyContinue'
+
+$version = '1.14.0'
+Write-Output "-Installing Docker Compose version: $version"
+
+$downloadUrl = "https://github.com/docker/compose/releases/download/$version/docker-compose-Windows-x86_64.exe"
+$outFilePath = "$env:TEMP\docker-compose.exe"
+Write-Output "--Downloading: $downloadUrl"
+Invoke-WebRequest -UseBasicParsing -OutFile $outFilePath -Uri $downloadUrl
+Move-Item $outFilePath "$env:ProgramFiles\docker\docker-compose.exe" -Force
+
+Write-Output '-Done'
+Write-Output '*'
+
+& docker-compose version
